ba36d6035ea8344c746b5a00a4040386de322ca2
paddyjcom-cpu <paddyj.com@gmail.com>
2026-01-11 00:33:02 +0000

    Initialize Supabase client in supabase.ts
    Supabase.ts

---
+++ services/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ufzeimniqplvniflycmg.supabase.co/';
+const supabaseKey = 'sb_publishable_mGzTyDJomZDJngwTKbdYQw_d5E0iJx3';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
