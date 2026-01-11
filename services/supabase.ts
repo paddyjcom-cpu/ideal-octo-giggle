bdefe38bbfa01ef17febb46521db9093576bc0bc
paddyjcom-cpu <paddyj.com@gmail.com>
2026-01-11 00:13:32 +0000

    Delete services directory

--- services/supabase.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://ufzeimniqplvniflycmg.supabase.co/';
-const supabaseKey = 'YOUR_SUPABASE_KEY_HERE';
-export const supabase = createClient(supabaseUrl, supabaseKey);
